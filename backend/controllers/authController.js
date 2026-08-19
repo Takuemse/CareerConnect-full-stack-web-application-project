5af5b9ab622d2e3381bdc65d5792202b9e45c252
Takuemse <takudzwamse@gmail.com>
2026-08-19 20:09:19 +0200

    Initial commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,104 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { createUser, findUserByEmail } = require('../queries/userQueries')
+
+const register = async(req, res) =>{
+    try {
+        const {name, password, email, role} = req.body;
+
+        if (!name || !email|| !password) {
+            return res.status(400).json({
+                message: "Name, email and password are required!"
+            })
+        }
+
+        const existingUser = await findUserByEmail(email)
+
+        if (existingUser) {
+            return res.status(409).json({
+                message: "Email already registered!"
+            })
+        }
+
+         const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await createUser(
+        name,
+        email,
+        hashedPassword,
+        role || "JOB_SEEKER"
+    )
+    res.status(201).json({
+        message: "User registered Successfully ",
+        user
+    })
+
+
+    
+
+        
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
+
+const login = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Email and password are required!"
+            })
+        }
+        const user = await findUserByEmail(email)
+
+        if (!user){
+            return res.status(401).json({
+                message: "Invalid email or password! "
+            })
+        }
+        const passwordMatch = await bcrypt.compare(
+            password,
+            user.password
+        )
+        if (!passwordMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password!"
+            })
+        }
+        const token = jwt.sign({
+            id: user.id,
+            role: user.role
+
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+        )
+        res.status(200).json({
+            message: "Login Successful",
+            token,
+            user: {
+               id: user.id,
+               name: user.name,
+               email: user.email,
+               role: user.role
+            }
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({
+            message: "Server error!"
+        })
+        
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('../backend/config/db')
+
+require('dotenv').config();
+
+const authRoutes = require('../backend/routes/authRoutes')
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/auth/', authRoutes)
+
+app.get('/', (req, res) =>{
+    res.json({
+        message: "CareerConnect is running on API"
+    })
+});
+
+app.get('/test-db', async (req, res) =>{
+    const result = await pool.query("SELECT NOW()")
+    res.json({
+        message: "Database connected",
+        time: result.rows[0]
+
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
